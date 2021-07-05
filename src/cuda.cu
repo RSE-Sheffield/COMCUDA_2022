70ea6559eb5a611c520a1683e0c7b651e59d3927
Robert Chisholm <R.Chisholm@sheffield.ac.uk>
2021-07-05 14:26:30 +0100

    Update helper comments in functions for students to complete.

--- src/cuda.cu
+++ src/cuda.cu
@@ -6,43 +6,35 @@ void cuda_begin(const Image *input_image) {
 }
 void cuda_stage1() {
     // Optionally during development call the skip function with the correct inputs to skip this stage
-    // You will need to copy the data back to host before passing to these functions
-    // int max_contrast = skip_histogram(&input_image, histograms)
+    // skip_tile_sum(input_image, mosaic_sum);
 
 #ifdef VALIDATION
     // TODO: Uncomment and call the validation function with the correct inputs
     // You will need to copy the data back to host before passing to these functions
     // (Ensure that data copy is carried out within the ifdef VALIDATION so that it doesn't affect your benchmark results!)
-    // validate_histogram(&input_image, histograms, max_contrast);
+    // validate_tile_sum(&input_image, mosaic_sum);
 #endif
 }
 void cuda_stage2(unsigned char* output_global_average) {
-    // Optionally during development call the skip function/s with the correct inputs to skip this stage
-    // You will need to copy the data back to host before passing to these functions
-    // skip_limited_histogram(TILES_X, TILES_Y, histograms, limited_histograms)
-    // skip_cumulative_histogram(TILES_X, TILES_Y, limited_histograms, cumulative_histograms)
-    // skip_equalised_histogram(TILES_X, TILES_Y, histograms, equalisied_histograms)
+    // Optionally during development call the skip function with the correct inputs to skip this stage
+    // skip_compact_mosaic(TILES_X, TILES_Y, mosaic_sum, compact_mosaic, global_pixel_average);
 
 #ifdef VALIDATION
     // TODO: Uncomment and call the validation functions with the correct inputs
-    // Note: Only validate_equalised_histogram() MUST be uncommented, the others are optional
     // You will need to copy the data back to host before passing to these functions
     // (Ensure that data copy is carried out within the ifdef VALIDATION so that it doesn't affect your benchmark results!)
-    // validate_limited_histogram(TILES_X, TILES_Y, histograms, test_limited_histograms);
-    // validate_cumulative_histogram(TILES_X, TILES_Y, limited_histograms, test_cumulative_histograms);
-    // validate_equalised_histogram(TILES_X, TILES_Y, histograms, test_equalisied_histograms);
+    // validate_compact_mosaic(TILES_X, TILES_Y, mosaic_sum, mosaic_value, output_global_average);
 #endif    
 }
 void cuda_stage3() {
     // Optionally during development call the skip function with the correct inputs to skip this stage
-    // You will need to copy the data back to host before passing to these functions
-    // skip_interpolate(&input_image, equalised_histograms, &output_image)
+    // skip_broadcast(input_image, compact_mosaic, output_image);
 
 #ifdef VALIDATION
     // TODO: Uncomment and call the validation function with the correct inputs
     // You will need to copy the data back to host before passing to these functions
     // (Ensure that data copy is carried out within the ifdef VALIDATION so that it doesn't affect your benchmark results!)
-    // validate_interpolate(&input_image, equalised_histograms, &output_image);
+    // validate_broadcast(&input_image, mosaic_value, &output_image);
 #endif    
 }
 void cuda_end(Image *output_image) {
